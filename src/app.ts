faceb9e31177a5f5c98ae5100b8e568af38c2e9c
“moviedo” <moviedo@grupokonecta.com>
2023-03-07 12:50:01 -0300

    test dayjs

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 import { DateFunctions, LogFunctions } from '.';
 import dayjs from 'dayjs';
-require('dayjs/locale/es');
-require('dayjs/locale/fr');
-require('dayjs/locale/de');
+import es = require('dayjs/locale/es');
+import fr = require('dayjs/locale/fr');
+import de = require('dayjs/locale/de');
 
-// import 'dayjs/esm/locale/ar';
-// const locale_de =  dayjs.locale('de') // use loaded locale globally
 
 const dd = DateFunctions.getDay_dd();
-//require('dayjs/locale/en'); // use locale globally
 
 LogFunctions.Log_Blue_Message_Console(
   `-------------------------------------------------------------------------------`,
@@ -17,7 +14,7 @@ LogFunctions.Log_Blue_Message_Console(
 // console.log(DateFunctions.getMonth_MM());
 // console.log(` DateFunctions.getDay_dd : ${dd}`);
 
-dayjs.locale('fr');
+dayjs.locale(fr);
 
 const dia = dayjs().format('dddd');
 console.log(dia);
@@ -25,11 +22,11 @@ console.log('---------------------------------------dddd MMMM YYYY -------------
 let msg = ` fr : ${dayjs().format('dddd MMMM YYYY')}`;
 console.log(msg);
 
-dayjs.locale('es');
+dayjs.locale(es);
 msg = ` es : ${dayjs().format('dddd MMMM YYYY')}`;
 console.log(msg);
 
-dayjs.locale('de');
+dayjs.locale(de);
 msg = ` de : ${dayjs().format('dddd MMMM YYYY')}`;
 //msg = ` de : ${dayjs().locale('de').toString()}\n`;
 console.log(msg);
